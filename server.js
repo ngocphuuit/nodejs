dd7e5ab0775006322cb721fed3a15eff9f9e58f6
ngocphuuit <ngocphu.uit@gmail.com>
2014-07-08 11:40:09 +0700

    nodejs server

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by NoPu on 7/8/14.
+ */
+var http = require('http');
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type': 'Text/html'});
+    res.write('hello nodejs');
+    res.end();
+    console.log('connecting ......');
+});
+server.listen(4567);
