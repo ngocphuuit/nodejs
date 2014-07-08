48484123f23d0a6bd4a8398212c0f9381fed9bd1
ngocphuuit <ngocphu.uit@gmail.com>
2014-07-08 11:42:04 +0700

    nodejs server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var server = http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type': 'Text/html'});
     res.write('hello nodejs');
     res.end();
-    console.log('connecting ......');
 });
+console.log('connecting ......');
 server.listen(4567);
